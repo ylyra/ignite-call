a8eaa6ce2ee2a8370ae05b1c9dddc67790d83689
Yan Lyra <yanlyra3@gmail.com>
2023-01-10 18:44:55 -0300

    feat: started connection with google

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,9 +1,17 @@
+import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
 
 import { global } from '../styles/global'
 
 global()
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
