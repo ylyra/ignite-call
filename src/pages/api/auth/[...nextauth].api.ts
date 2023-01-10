569a6981a1cc669b2abbaa450ca27d284105f232
Yan Lyra <yanlyra3@gmail.com>
2023-01-10 18:51:54 -0300

    feat: added required scopes to authenticate with google

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -1,15 +1,33 @@
 import NextAuth, { AuthOptions } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
+const GOOGLE_CALENDAR_SCOPE =
+  'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar'
+
 export const authOptions: AuthOptions = {
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          scope: GOOGLE_CALENDAR_SCOPE,
+        },
+      },
     }),
     // ...add more providers here
   ],
+
+  callbacks: {
+    async signIn({ account }) {
+      if (!account?.scope?.includes(GOOGLE_CALENDAR_SCOPE)) {
+        return '/register/connect-calendar?error=permissions'
+      }
+
+      return true
+    },
+  },
 }
 
 export default NextAuth(authOptions)
