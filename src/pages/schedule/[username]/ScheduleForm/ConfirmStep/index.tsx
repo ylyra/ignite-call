ac3189f3c25aaec9bb6762016c2839d034cc1de5
Yan Lyra <yan.lyra@growth.sale>
2023-01-15 02:17:58 -0300

    feat: finished frontend for confirmation step

--- src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -1,4 +1,47 @@
-export function ConfirmStep() {
-  return <div />;
-}
+import { Button, Text, TextArea, TextInput } from "@ignite-ui/react";
+import { CalendarBlank, Clock } from "phosphor-react";
+import { ConfirmForm, FormActions, FormHeader } from "./styles";
 
+export function ConfirmStep() {
+  const onConfirmSchedulingSubmit = () => {
+    console.log("confirm");
+  };
+
+  return (
+    <ConfirmForm as="form">
+      <FormHeader>
+        <Text>
+          <CalendarBlank />
+          22 de abril de 2021
+        </Text>
+
+        <Text>
+          <Clock />
+          10:00
+        </Text>
+      </FormHeader>
+
+      <label>
+        <Text size="sm">Nome completo</Text>
+        <TextInput placeholder="Seu nome" />
+      </label>
+
+      <label>
+        <Text size="sm">Endereço de e-mail</Text>
+        <TextInput type="email" placeholder="johndoe@example.com" />
+      </label>
+
+      <label>
+        <Text size="sm">Observações</Text>
+        <TextArea />
+      </label>
+
+      <FormActions>
+        <Button type="button" variant="tertiary">
+          Cancelar
+        </Button>
+        <Button type="submit">Confirmar</Button>
+      </FormActions>
+    </ConfirmForm>
+  );
+}
